549d3135e53b3902ca93bd11a910f894fe383b80
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-02-05 07:03:13 +0900

    공백열과 단일값 열 제거 전처리 추가

---
+++ preprocessing/RemoveEmptyColumn.py
@@ -0,0 +1,30 @@
+import pandas as pd
+
+
+def preprocess(train_X, test):
+    """원핫 인코딩
+
+    Parameters
+    ----------
+    train_X : df.DataFrame
+        학습할 데이터셋의 데이터프레임
+
+    test : df.DataFrame
+        추론할 데이터셋의 데이터프레임
+
+    Returns
+    -------
+    train_X : df.DataFrame
+        인코딩된 학습 데이터프레임
+    test : df.DataFrame
+        인코딩된 추론 데이터프레임
+    
+    Notes
+    -----
+    이 함수는 모든행이 na인 열을 제거
+    """
+
+    train_X = train_X.dropna(axis=1, how='all')
+    test = test[train_X.columns]
+
+    return train_X, test
---
+++ preprocessing/RemoveStdZeroColumn.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+
+def preprocess(train_X, test):
+    """원핫 인코딩
+
+    Parameters
+    ----------
+    train_X : df.DataFrame
+        학습할 데이터셋의 데이터프레임
+
+    test : df.DataFrame
+        추론할 데이터셋의 데이터프레임
+
+    Returns
+    -------
+    train_X : df.DataFrame
+        인코딩된 학습 데이터프레임
+    test : df.DataFrame
+        인코딩된 추론 데이터프레임
+    
+    Notes
+    -----
+    이 함수는 열의 표준편차가 0인 열을 제거
+    """
+
+    desc = train_X.describe()
+    std_zero_col = desc.columns[desc.loc['std'] == 0]
+    train_X = train_X.drop(std_zero_col, axis=1)
+    test = test.drop(std_zero_col, axis=1)
+
+    return train_X, test
