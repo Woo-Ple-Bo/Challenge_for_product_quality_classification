c27ecc96f31ff8e5271bb0efadaa531a6f3a992f
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-02-07 15:31:50 +0900

    제품코드와 생산라인 결합 전처리

---
+++ preprocessing/ConcatProdLine.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+
+def preprocess(train_X, test):
+    """원핫 인코딩
+
+    Parameters
+    ----------
+    train_X : df.DataFrame
+        학습할 데이터셋의 데이터프레임
+
+    test : df.DataFrame
+        추론할 데이터셋의 데이터프레임
+
+    Returns
+    -------
+    train_X : df.DataFrame
+        인코딩된 학습 데이터프레임
+    test : df.DataFrame
+        인코딩된 추론 데이터프레임
+    
+    Notes
+    -----
+    PRODUCT_CODE 열과 LINE 열을 이어냄
+    """
+
+    train_X['PROD_LINE'] = train_X['PRODUCT_CODE']+'_'+train_X['LINE']
+    train_X = train_X.drop(['PRODUCT_CODE','LINE'],axis=1)
+    test['PROD_LINE'] = test['PRODUCT_CODE']+'_'+test['LINE']
+    test = test.drop(['PRODUCT_CODE','LINE'],axis=1)
+
+    return train_X, test, ['PROD_LINE']
